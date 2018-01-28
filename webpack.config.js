7f9930df7c61fb45b3edafc302853f5498fef5a1
rchaser53 <tayoshizawa29@gmail.com>
2018-01-29 00:00:24 +0900

    add tslint config

--- webpack.config.js
+++ webpack.config.js
@@ -14,21 +14,34 @@ module.exports = {
 	},
 	module: {
 		rules: [
-			{
-				test: /\.ts$/,
-				exclude: /node_modules|vue\/src/,
-				loader: 'ts-loader',
-				options: {
-					appendTsSuffixTo: [/\.vue$/]
-				}
-			},
-			{
-				test: /\.vue$/,
-				loader: 'vue-loader',
-				options: {
-					esModule: true
-				}
-			}
+            {
+                enforce: 'pre',
+                test: /\.ts$/,
+                loader: 'tslint-loader',
+                exclude: /(node_modules)/,
+                options: {
+                    configFile: 'tslint.json'
+                }
+            },
+            {
+                test: /\.ts$/,
+                exclude: /node_modules|vue\/src/,
+                loader: 'ts-loader',
+                options: {
+                    appendTsSuffixTo: [/\.vue$/],
+                    transpileOnly: true,
+                    // isolatedModules: true
+                }
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        ts: 'ts-loader!tslint-loader'
+                    }
+                }
+            }
 		]
 	},
 	devServer: {
