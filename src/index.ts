84120d7f823baca73f58e7244a64995319306be9
rchaser53 <tayoshizawa29@gmail.com>
2018-02-12 14:33:04 +0900

    fix config

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import Vue from 'vue'
-// import ComponentA from './components/componentA.vue'
+import ComponentA from './components/componentA.vue'
 
-import ComponentA from './components/av-ts-component.vue'
+// import ComponentA from './components/av-ts-component.vue'
 
 new Vue({
-
-	      el: "#appaaa",
+	el: '#appaaa',
 	components: { ComponentA },
 	template: '<component-a my-prop="{a:1, b:3}"/>'
 })
